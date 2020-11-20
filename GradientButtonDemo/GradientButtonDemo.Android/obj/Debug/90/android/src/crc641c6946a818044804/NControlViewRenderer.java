e9386bd5c9a757a1fa0dd6009cdaac829b42e358
feed755x <feed755x@gmail.com>
2020-11-19 20:31:20 -0600

    Gradient Button

---
+++ GradientButtonDemo/GradientButtonDemo.Android/obj/Debug/90/android/src/crc641c6946a818044804/NControlViewRenderer.java
@@ -0,0 +1,72 @@
+package crc641c6946a818044804;
+
+
+public class NControlViewRenderer
+	extends crc643f46942d9dd1fff9.VisualElementRenderer_1
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_draw:(Landroid/graphics/Canvas;)V:GetDraw_Landroid_graphics_Canvas_Handler\n" +
+			"n_onTouchEvent:(Landroid/view/MotionEvent;)Z:GetOnTouchEvent_Landroid_view_MotionEvent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("NControl.Droid.NControlViewRenderer, NControl.Droid", NControlViewRenderer.class, __md_methods);
+	}
+
+
+	public NControlViewRenderer (android.content.Context p0, android.util.AttributeSet p1, int p2)
+	{
+		super (p0, p1, p2);
+		if (getClass () == NControlViewRenderer.class)
+			mono.android.TypeManager.Activate ("NControl.Droid.NControlViewRenderer, NControl.Droid", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android:System.Int32, mscorlib", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public NControlViewRenderer (android.content.Context p0, android.util.AttributeSet p1)
+	{
+		super (p0, p1);
+		if (getClass () == NControlViewRenderer.class)
+			mono.android.TypeManager.Activate ("NControl.Droid.NControlViewRenderer, NControl.Droid", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public NControlViewRenderer (android.content.Context p0)
+	{
+		super (p0);
+		if (getClass () == NControlViewRenderer.class)
+			mono.android.TypeManager.Activate ("NControl.Droid.NControlViewRenderer, NControl.Droid", "Android.Content.Context, Mono.Android", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void draw (android.graphics.Canvas p0)
+	{
+		n_draw (p0);
+	}
+
+	private native void n_draw (android.graphics.Canvas p0);
+
+
+	public boolean onTouchEvent (android.view.MotionEvent p0)
+	{
+		return n_onTouchEvent (p0);
+	}
+
+	private native boolean n_onTouchEvent (android.view.MotionEvent p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ GradientButtonDemo/GradientButtonDemo/GradientButton.cs
@@ -0,0 +1,178 @@
+﻿using MagicGradients;
+using NControl.Abstractions;
+using NGraphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Rect = NGraphics.Rect;
+
+namespace GradientButtonDemo
+{
+    class GradientButton : NControlView
+    {
+        private Frame _frame;
+        private Label _label;
+        private GradientView _gradientView;
+        public float BorderRadius
+        {
+            get => (float)GetValue(BorderRadiusProperty); 
+            set
+            {
+                SetValue(BorderRadiusProperty, value);
+                Invalidate();
+            }
+        }
+        static float initialRadius = 15;
+
+        public static BindableProperty BorderRadiusProperty =
+            BindableProperty.Create(nameof(BorderRadius),
+                typeof(float),
+                typeof(GradientButton),
+                initialRadius,
+                propertyChanged: (b, o, n) =>
+                { 
+                    var control = (GradientButton)b;
+                    control.BorderRadius = (float)n;
+                });
+
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set
+            {
+                SetValue(TextProperty, value);
+                Invalidate();
+            }
+        }
+
+        public static BindableProperty TextProperty =
+            BindableProperty.Create(nameof(Text),
+                typeof(string),
+                typeof(GradientButton),
+                "",
+                propertyChanged: (b, o, n) =>
+                {
+                    var control = (GradientButton)b;
+                    control.Text = (string)n;
+                });
+
+        public string GradientStyle
+        {
+            get => (string)GetValue(GradientStyleProperty);
+            set
+            {
+                SetValue(GradientStyleProperty, value);
+                Invalidate();
+            }
+        }
+
+        public static BindableProperty GradientStyleProperty =
+            BindableProperty.Create(nameof(GradientStyle),
+                typeof(string),
+                typeof(GradientButton),
+                "linear-gradient(37deg, rgb(32, 218, 233),rgb(40, 21, 236))",
+                propertyChanged: (b, o, n) =>
+                {
+                    var control = (GradientButton)b;
+                    control.GradientStyle = (string)n;
+                });
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set
+            {
+                SetValue(CommandProperty, value);
+                Invalidate();
+            }
+        }
+
+        public static BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(Command),
+                typeof(ICommand),
+                typeof(GradientButton),
+                defaultBindingMode:BindingMode.TwoWay,
+                propertyChanged: (b, o, n) =>
+                {
+                    var control = (GradientButton)b;
+                    control.Command = (ICommand)n;
+                });
+
+        public GradientButton() 
+        {
+            _label = new Label
+            {
+                Text = "Hola",
+                TextColor = Xamarin.Forms.Color.White,
+                VerticalTextAlignment = Xamarin.Forms.TextAlignment.Center,
+                HorizontalTextAlignment = Xamarin.Forms.TextAlignment.Center
+            };
+
+            _gradientView = new GradientView
+            {
+                GradientSource = new CssGradientSource
+                { 
+                    Stylesheet = "linear-gradient(37deg, rgb(32, 218, 233),rgb(40, 21, 236))"
+                }
+            };
+
+            _frame = new Frame
+            {
+                Content = new Grid
+                {
+                    Children =
+                   {
+                        _gradientView,
+                        _label
+                   }
+                },
+                Padding = 0,
+                CornerRadius = 15f
+            };
+
+            Content = _frame;
+        }
+
+        public override bool TouchesBegan(IEnumerable<NGraphics.Point> points)
+        {
+            this.ScaleTo(0.96, 65, Easing.CubicInOut);
+            return true;
+        }
+
+        public override bool TouchesCancelled(IEnumerable<NGraphics.Point> points)
+        {
+            this.ScaleTo(1, 65, Easing.CubicInOut);
+            return true;
+        }
+
+        public override bool TouchesEnded(IEnumerable<NGraphics.Point> points)
+        {
+            this.ScaleTo(1, 65, Easing.CubicInOut);
+            if (Command != null && Command.CanExecute(null)) 
+            {
+                Command.Execute(null);
+            }
+            return true;
+        }
+
+        public override void Draw(ICanvas canvas, Rect rect)
+        {
+            /**
+             *             canvas.DrawLine(rect.Left, rect.Top, rect.Width, rect.Height,
+                            NGraphics.Colors.Red);
+            canvas.DrawLine(rect.Width, rect.Top, rect.Left, rect.Height,
+                NGraphics.Colors.Green);**/
+
+            _frame.CornerRadius = BorderRadius;
+            _label.Text = Text;
+            _gradientView.GradientSource = new CssGradientSource { 
+                Stylesheet = this.GradientStyle
+            };
+        }
+
+
+    }
+}
